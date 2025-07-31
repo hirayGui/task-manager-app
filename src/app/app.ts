3ee49cc6dbab807baa08843437f5273c7f2911e1
hiraygui <hiraygui@gmail.com>
2025-07-31 15:44:11 -0300

    feat(Header): Construindo header da pagina principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: 'app-root', // nome do componente
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.html', // conteudo do componente
+  styleUrl: './app.css' // estilizacao do componente
 })
 export class App {
   protected readonly title = signal('first-angular-app');
