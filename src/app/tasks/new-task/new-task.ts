591475b9f432f38e9e00886b3b7a021d7c28286a
hiraygui <hiraygui@gmail.com>
2025-08-25 15:03:04 -0300

    feat(Tasks): Adicionando função para criar novas tarefas para um determinado usuário

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,15 +1,35 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  
+  // exemplo com a utilização de Signals. A grosso modo falando, apenas a declaração das variáveis mudaria.
+  /* title = signal('');
+  summary = signal('');
+  dueDate = signal(''); */
+
+  title = '';
+  summary = '';
+  dueDate = '';
 
   onCancelAddTask(){
       this.cancel.emit();
+  }
+
+  onSubmit(){
+    this.add.emit({
+      title: this.title,
+      summary: this.summary,
+      date: this.dueDate,
+    });
   }
 }
